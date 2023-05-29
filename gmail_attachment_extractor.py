dc3563fdb19f14981e1b7d1238f63046244dbac6
kuanmy <kuan.mengyi@gmail.com>
2023-05-29 08:45:15 +0800

    Get gmails with attachments

--- gmail_attachment_extractor.py
+++ gmail_attachment_extractor.py
@@ -2,6 +2,7 @@ import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -9,6 +10,8 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 class GmailAttachmentExtractor:
     def __init__(self) -> None:
         self.set_creds()
+        self.service = build('gmail', 'v1', credentials=self.creds)
+
 
     def set_creds(self):
         """ Get and set the credentials for authenticating the Gmail API service """
@@ -29,5 +32,22 @@ class GmailAttachmentExtractor:
             with open('token.json', 'w') as token:
                 token.write(self.creds.to_json())
 
+
+    def get_message_ids(self, query='', labelIds=[]):
+        """ Get message ids that fulfill the specified query condition, and comes with attachments """
+        query += ' has:attachment'
+        results = self.service.users().messages().list(userId='me',q=query, labelIds=labelIds, fields='messages(id),nextPageToken').execute()
+        messages = []
+        if 'messages' in results:
+            messages.extend(list(map(lambda msg: msg['id'], results['messages'])))
+        while 'nextPageToken' in results:
+            page_token = results['nextPageToken']
+            results = self.service.users().messages().list(userId='me',q=query, pageToken=page_token, fields='messages(id),nextPageToken').execute()
+            if 'messages' in results:
+                messages.extend(list(map(lambda msg: msg['id'], results['messages'])))
+        return messages
+
+
 if __name__ == '__main__':
     extractor = GmailAttachmentExtractor()
+    message_ids = extractor.get_message_ids()
