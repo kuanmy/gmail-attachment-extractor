83d5027d567420920ab488b815ae9254361a8783
kuanmy <kuan.mengyi@gmail.com>
2023-05-29 08:36:37 +0800

    Authorize gmail API service

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# gmail-attachment-extractor
+# Gmail Attachment Extractor
+
+1. Follow Gmail API documentation to setup environment
+    https://developers.google.com/gmail/api/quickstart/python
+    - Enable the API
+    - Authorize credentials for a desktop application
+2. Install dependencies
+    ```
+    pip install -r requirements.txt
+    ```
---
+++ gmail_attachment_extractor.py
@@ -0,0 +1,33 @@
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+class GmailAttachmentExtractor:
+    def __init__(self) -> None:
+        self.set_creds()
+
+    def set_creds(self):
+        """ Get and set the credentials for authenticating the Gmail API service """
+        self.creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(self.creds.to_json())
+
+if __name__ == '__main__':
+    extractor = GmailAttachmentExtractor()
