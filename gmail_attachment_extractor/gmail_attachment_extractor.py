6d22cfb8d461bc26eece26bede4013623717bb3b
kuanmy <kuan.mengyi@gmail.com>
2023-11-27 10:48:18 +0800

    Simplify code

--- gmail_attachment_extractor/gmail_attachment_extractor.py
+++ gmail_attachment_extractor/gmail_attachment_extractor.py
@@ -6,14 +6,12 @@ from logger import CsvLogger, TxtLogger
 from typing import List
 
 
-def extract_gmail_attachments(output_atch_dir: str = '', output_record_log_path: str = 'record_log', output_error_log_path: str = 'error_log', processed_record_log_paths: List[str] = []) -> None:
+def extract_gmail_attachments(output_atch_dir: str, output_record_log_path: str, output_error_log_path: str, processed_record_log_path: str) -> None:
     service = GmailService()
     all_message_ids = service.get_message_ids()
     message_ids = all_message_ids
     
-    processed_message_ids = []
-    for processed_record_log_path in processed_record_log_paths:
-        processed_message_ids.extend(TxtLogger.read(processed_record_log_path + '.txt'))
+    processed_message_ids = TxtLogger.read(processed_record_log_path + '.txt')
     processed_message_ids = set(processed_message_ids)
     message_ids = [message_id for message_id in all_message_ids if message_id not in processed_message_ids]
 
@@ -26,8 +24,6 @@ def extract_gmail_attachments(output_atch_dir: str = '', output_record_log_path:
             CsvLogger.write(output_error_log_path + '.csv', [message.id, "Module Ref No not found for subject: " + message.subject])
         else:
             (module, ref_no) = module_ref_no
-            if module == 'Payment Service':
-                module = 'PMS'
         for attachment in message.attachments:
             attachment.save(dir=os.path.join(output_atch_dir, module, ref_no))
         TxtLogger.write(output_record_log_path + '.txt', message.id)
@@ -38,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output-atch-dir', dest='output_atch_dir', default='')
     parser.add_argument('--output-record-log-path', dest='output_record_log_path', default='record_log')
     parser.add_argument('--output-error-log-path', dest='output_error_log_path', default='error_log')
-    parser.add_argument('--processed-record-log-path', dest='processed_record_log_path', default=[])
+    parser.add_argument('--processed-record-log-path', dest='processed_record_log_path', default='record_log')
     args = parser.parse_args()
 
     extract_gmail_attachments(args.output_atch_dir, args.output_record_log_path, args.output_error_log_path, args.processed_record_log_path)
--- gmail_attachment_extractor/message.py
+++ gmail_attachment_extractor/message.py
@@ -16,22 +16,10 @@ class Message:
 
     
     def get_module_ref_no(self) -> Optional[Tuple[str, str]]:
-        patterns = [
-            # 1st pattern: [moduleShortForm] #[refNo]
-            (r"(RQ|PR|PO|PRQ|Payment Service) #\S*", " #"),
-            # 2nd pattern: [moduleShortForm] Approval of [refNo]
-            (r"(RQ|PR|PO|PRQ) Approval of \S*", " Approval of "),
-            # 3rd pattern: [moduleShortForm] Approval [refNo]
-            (r"(RQ|PR|PO|PRQ) Approval \S*", " Approval "),
-            # 4th pattern: [moduleShortForm] Reviewer [refNo]
-            (r"(RQ|PR|PO|PRQ) Reviewer \S*", " Reviewer ")
-        ]
-
-        for pattern, split_key in patterns:
-            matching_substring = re.search(pattern, self.subject)
-            if matching_substring is not None:
-                module_ref_no = matching_substring.group().split(split_key)
-                return tuple(module_ref_no)
-
-        return None
+        matching_substring = re.search(r"(RQ|PR|PO|PRQ #\S*")
+        if matching_substring is not None:
+            module_ref_no = matching_substring.group().split(" #")
+            return tuple(module_ref_no)
+        else:
+            return None
 
